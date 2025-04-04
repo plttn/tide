4498f4685b307c8f827325f36a8898d9a736144b
Jack Platten <jack@platten.me>
2025-04-04 14:38:43 -0700

    Revert "Update _tide_item_rustc.fish"

--- functions/_tide_item_rustc.fish
+++ functions/_tide_item_rustc.fish
@@ -1,6 +1,6 @@
 function _tide_item_rustc
     if path is $_tide_parent_dirs/Cargo.toml
-        rustup show active-toolchain | string match -qr "(?<v>^[\d.]+)"
+        rustc --version | string match -qr "(?<v>[\d.]+)"
         _tide_print_item rustc $tide_rustc_icon' ' $v
     end
 end
