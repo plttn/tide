dff767b7b3d7067cbf27e9d968466d3e33e3ce45
Jack Platten <jack@platten.me>
2026-04-18 14:45:10 -0700

    Fix vcs separator colors by printing git/jj styles

--- functions/tide/configure/functions/_fake_tide_item_vcs.fish
+++ functions/tide/configure/functions/_fake_tide_item_vcs.fish
@@ -1,3 +1,3 @@
 function _fake_tide_item_vcs
-    _fake_tide_print_item vcs (set_color $fake_tide_git_color_branch) $fake_tide_git_icon' ' main
+    _fake_tide_print_item git (set_color $fake_tide_git_color_branch) $fake_tide_git_icon' ' main
 end
