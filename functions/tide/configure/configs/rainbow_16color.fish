92a058d387e64df7ad0e58656dad669ca30f7e50
Jack Platten <jack@platten.me>
2026-07-21 12:48:25 -0700

    fix(sudo): Update colors for sudo indicator

--- functions/tide/configure/configs/rainbow_16color.fish
+++ functions/tide/configure/configs/rainbow_16color.fish
@@ -75,7 +75,7 @@ tide_status_bg_color black
 tide_status_bg_color_failure red
 tide_status_color green
 tide_status_color_failure bryellow
-tide_sudo_bg_color white
+tide_sudo_bg_color yellow
 tide_sudo_color black
 tide_terraform_bg_color magenta
 tide_terraform_color black
