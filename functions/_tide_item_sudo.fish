b3d5551cf8a00636abe16e9b5614f2945038f0ad
Jack Platten <jack@platten.me>
2026-07-21 11:42:07 -0700

    fix(sudo): Exclude root from displaying sudo indicator

--- functions/_tide_item_sudo.fish
+++ functions/_tide_item_sudo.fish
@@ -1,5 +1,3 @@
 function _tide_item_sudo
-    if sudo -n true 2>/dev/null
-        _tide_print_item sudo $tide_sudo_icon
-    end
+    test "$EUID" != 0 && sudo -n true 2>/dev/null && _tide_print_item sudo $tide_sudo_icon
 end
